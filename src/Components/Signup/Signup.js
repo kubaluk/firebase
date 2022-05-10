8630b984d023efcb09567219f50caab859d991bf
gamedevturdus <kubaluk757@o2.pl>
2022-05-10 15:44:22 +0200

    re-added theme button to signup page

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { StyledSignup } from '../../Styles/StyledSignup'
 import SignupForm from './SingupForm'
+import ThemeButton from '../ThemeButton/ThemeButton'
 
 function Signup(){
   return (
     <>
+      <ThemeButton />
       <StyledSignup>
         <SignupForm />
       </StyledSignup>
--- src/Styles/StyledThemeButton.js
+++ src/Styles/StyledThemeButton.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components'
 
 const StyledThemeButton = styled.div`
+  position: fixed;
+  top: 5%;
+  left: 5%;
   font-size: 2rem;
 `
 
